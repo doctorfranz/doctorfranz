01159ade213599b32b362388cb854340ff7d0b4e
Dean Franz <rfranz1@uthsc.edu>
2023-03-11 14:01:09 -0600

    fixed progress bar

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ window.onload = function() {
   var progressBar = document.querySelector(".progress-bar");
 
   var width = 0;
-  var interval = setInterval(function() {
-    if (width >= 100) {
-      clearInterval(interval);
-    } else {
-      width += 10;
-      progressBar.style.width = width + "%";
-    }
-  }, 500);
+  var interval;
+
+  function startInterval() {
+    interval = setInterval(function() {
+      if (width >= 100) {
+        width = 0;
+      } else {
+        width += 10;
+        progressBar.style.width = width + "%";
+      }
+    }, 500);
+  }
+
+  startInterval();
 }
